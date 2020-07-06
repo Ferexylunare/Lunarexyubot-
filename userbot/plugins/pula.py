ff7fe723f428285aeac214c75667ed15429c197c
Kazutettoh <66389612+Kazutettoh@users.noreply.github.com>
2020-07-06 22:40:10 +0200

    Create pula.py

---
+++ userbot/plugins/pula.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import asyncio
+
+from telethon import events
+from userbot import CMD_HELP, ALIVE_NAME, bot
+from userbot.system import dev_cmd
+from platform import uname
+
+
+@bot.on(dev_cmd(pattern=r"pula"))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.3
+    animation_ttl = range(0, 12)
+    await event.edit("🚨 👮‍")
+    animation_chars = [
+        await asyncio.wait([e.edit("🔵🔵🔵🔵   🔴🔴🔴🔴\n🔵🔵🔵🔵   🔴🔴🔴🔴\n🔵🔵🔵🔵   🔴🔴🔴🔴\n🔵🔵🔵🔵   🔴🔴🔴🔴")])
+    await asyncio.sleep(0.1)
+    await asyncio.wait([e.edit("🔴🔴🔴🔴   🔵🔵🔵🔵\n🔴🔴🔴🔴   🔵🔵🔵🔵\n🔴🔴🔴🔴   🔵🔵🔵🔵\n🔴🔴🔴🔴   🔵🔵🔵🔵")])
+            f"{DEFAULTUSER} **🚨🚔 CHIAMATE IL CIENTODICIOTTOO!! 🚔🚨**"
